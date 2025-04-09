fd265ff2eb1670728898685a1ec97431185a8ebb
Prad N <prad@sonr.io>
2025-04-08 20:51:59 -0400

    feat: implement time-based UI component in native example

---
+++ examples/native/go.mod
@@ -0,0 +1,12 @@
+module github.com/sonr-io/cosmstyle/example
+
+replace github.com/sonr-io/cosmstyle => ../
+
+go 1.24.2
+
+require (
+	github.com/a-h/templ v0.3.857
+	github.com/sonr-io/cosmstyle v0.0.0-00010101000000-000000000000
+)
+
+require github.com/gomarkdown/markdown v0.0.0-20250311123330-531bef5e742b // indirect
---
+++ examples/native/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/sonr-io/cosmstyle/example/pages"
+
+	"github.com/a-h/templ"
+)
+
+func main() {
+	http.Handle("/", templ.Handler(pages.TimeComponent(time.Now())))
+	http.Handle("/404", templ.Handler(pages.NotFoundComponent(), templ.WithStatus(http.StatusNotFound)))
+	http.ListenAndServe(":7654", nil)
+}
